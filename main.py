bd8ebbb7a7899db176e67fe6f6303b6eba45ba9c
Garryybd <118138251+GarryyBD@users.noreply.github.com>
2023-09-23 15:14:59 -0400

    15 lines of code

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
+# pylint: disable-all
 import time
 import discord
 from dotenv import dotenv_values
 
 class MyClient(discord.Client):
-    """
-    Bot class
-    """
     async def on_ready(self):
-        """
-        When the bot is ready
-        """
-        print('Logged on as', self.user)
-
         channel = self.get_channel(int(config["CHANNEL_ID"]))
-
         while True:
             await channel.send(config["MESSAGE"])
             time.sleep(60)
-
 
 client = MyClient()
 config = dotenv_values(".env")
